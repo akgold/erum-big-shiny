16a1b8388d6c70b8d26cf1757f2a5921e62d5129
Alex Gold <alexkgold@gmail.com>
2020-06-15 17:30:49 -0400

    Add r6

--- app/app.R
+++ app/app.R
@@ -4,6 +4,7 @@ library(httr)
 library(ggplot2)
 library(dplyr)
 library(leaflet)
+library(bikeR6pkg)
 
 # Create dashboard page UI
 ui <- dashboardPage(skin = "red",
@@ -15,11 +16,10 @@ ui <- dashboardPage(skin = "red",
                         leafletOutput(
                           "map"
                         ),
-                        shiny::actionButton(
+                        actionButton(
                           "refresh",
-                          label = "Refresh",
-                          icon("refresh")
-                        ),
+                          "Refresh",
+                          icon = icon('refresh')),
                         verbatimTextOutput(
                           "last_updated"
                         ),
@@ -29,84 +29,38 @@ ui <- dashboardPage(skin = "red",
 )
 
 server <- function(input, output, session) {
+  api_client <- BikeClient$new()
 
-  #----- Data Gathering-----
-  my_api <- "https://colorado.rstudio.com/rsc/erum-bikes/"
-
-  # Get URLS of feeds
-  feeds <- file.path(my_api, "feeds") %>%
-    httr::GET() %>%
-    httr::content() %>%
-    purrr::map_df(tibble::as.tibble)
-
-  # Get URL of station feed
-  station_url <- feeds %>%
-    dplyr::filter(name == "station_information") %>%
-    dplyr::pull(url)
-
-  # Get stations (name, lat, long)
-  stations <- file.path(my_api, "feed_dat") %>%
-    httr::GET(
-      query = list(
-        feed_url = station_url
-      )
-    ) %>%
-    httr::content() %>%
-    magrittr::extract2("dat") %>%
-    purrr::map_df(tibble::as.tibble)
-
-  # Get URL of current status feed
-  status_url <- feeds %>%
-    dplyr::filter(name == "station_status") %>%
-    dplyr::pull(url)
-
-  # Get current status (bikes/docks per station)
-  status_dat <- reactive({
+  #----- Data Gathering + Plot Data Formatting -----
+  dat <- reactive({
     input$refresh
 
-    status <- file.path(my_api, "feed_dat") %>%
-      httr::GET(
-        query = list(
-          feed_url = status_url
-        )
-      ) %>%
-      httr::content()
-
-    status$dat <- status$dat %>%
-      purrr::map_df(tibble::as.tibble)
-    status$last_updated <- status$last_updated[[1]]
-
-    status
+    api_client$update_status()
+    list(
+      last_updated = api_client$last_updated,
+      status = api_client$status
+    )
   })
 
-  #----- Create Plot Data -----
+  output$last_updated <- renderText(dat()$last_updated)
+
   plot_dat <- reactive({
-    inner_join(stations, status_dat()$dat) %>%
-      mutate(
-        plot_val = glue::glue(
-          paste0(
-            "<b>{name}</b><br>",
-            "Bikes: {num_bikes_available}<br>",
-            "Empty Docks: {num_docks_available}"
-          ))
-      )
+    req(dat)
+    bikeR6pkg::make_plot_dat(api_client$stations, dat()$status)
   })
 
   #----- Format Outputs -----
-  output$last_updated <- renderText({
-    req(plot_dat)
-    glue::glue("Last Updated: {status_dat()$last_updated}")
-  })
-
   output$map <- renderLeaflet({
+    req(plot_dat)
+
     plot_dat() %>%
       leaflet() %>%
       addProviderTiles(
         providers$CartoDB.Positron
       ) %>%
       setView(
-        lng = median(stations$lon),
-        lat = median(stations$lat),
+        lng = median(plot_dat()$lon),
+        lat = median(plot_dat()$lat),
         zoom = 14) %>%
       addAwesomeMarkers(
         lng = ~lon,
